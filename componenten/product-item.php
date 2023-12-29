0b7f37dbf9e0b29694a259f9627d18284e58ed35
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-12-29 19:54:34 +0100

    wip

--- componenten/product-item.php
+++ componenten/product-item.php
@@ -2,7 +2,7 @@
 <div class="product-item h-fit relative">
         <a class="w-full flex justify-center" href="<?php the_permalink(); ?>">
         <div class="aspect-square w-full bg-[#F2F4F5] flex justify-center items-center rounded-[10px] overflow-hidden relative">
-            <img src="<?php echo get_the_post_thumbnail_url($product->get_id()); ?>" alt="" class="h-ful w-auto max-h-[80px] md:max-h-[150px] px-2 mix-blend-multiply">
+            <img src="<?php echo get_the_post_thumbnail_url($product->get_id()); ?>" alt="" class="h-[65%] w-auto max-h-[calc(100%-80px)] md:max-h-[calc(100%-100px)] px-2 mix-blend-multiply">
             <div class="absolute top-[4px] right-[4px]"><?php echo do_shortcode('[yith_wcwl_add_to_wishlist]'); ?></div>
             <div class="absolute bottom-[8px] right-[8px] h-3 w-3 rounded-full bg-[#C2F0A0] flex items-center justify-center font-jakarta text-15 leading-15 font-extrabold text-[#000]"><span class="mb-[5px]">+</span></div>
         </div>
