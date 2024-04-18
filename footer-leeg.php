bad4f3dee3b49bd894b0628c77e2edc3444d218e
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-04-18 19:56:37 +0200

    wip

---
+++ footer-leeg.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * The template for displaying the footer
+ * 
+ * @package Day Six theme
+ */
+ ?>
+
+<footer>
+
+   
+</footer>
+<?php include get_template_directory() . '/componenten/side-cart.php'; ?>
+<?php wp_footer(); ?>
+</body>
+</html>
