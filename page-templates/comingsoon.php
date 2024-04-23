ce89a174fdf3b46cb13a777490f155d20cdc16c3
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-04-23 15:57:23 +0200

    wip

--- page-templates/comingsoon.php
+++ page-templates/comingsoon.php
@@ -7,7 +7,7 @@
  get_header(); ?>
 
 <main>
-   <a href="/home" class="fixed top-0 left-0 right- bottom-0 bg-white z-[9999]"></a>
+   <a href="/home" class="fixed top-0 left-0 right-0 bottom-0 bg-white z-[9999]"></a>
 </main>
 <?php get_footer('leeg'); ?>
 
